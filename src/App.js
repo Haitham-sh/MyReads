eda4df0c8dc2e0f1c2d84604c16a1b510f81435d
HAITHAM MOHAMED MOHAMED ELSHERBINY <91070657+Haitham-sh@users.noreply.github.com>
2022-05-24 01:29:45 +0200

    break the code to home page and search page by React Router

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import {Routes, Route } from "react-router-dom";
+import Home from './Home';
+import Search from './Search';
+
+function App() {
+
+  return (
+    <Routes>
+      <Route
+          exact path="/"
+          element={<Home />}>
+      </Route>
+      <Route
+          exact path="/search"
+          element={<Search />}>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/Search.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import ApiData from './ApiData'
+
+function Search() {
+  return (
+    <div className="search-books">
+    <div className="search-books-bar">
+      <Link
+        to="/"
+        className="close-search"
+      >
+        Close
+      </Link>
+      <div className="search-books-input-wrapper">
+        <input
+          type="text"
+          placeholder="Search by title, author, or ISBN"
+        />
+      </div>
+    </div>
+    <div className="search-books-results">
+                <ol className="books-grid"><ApiData/></ol>
+    </div>
+  </div>
+  )
+}
+
+export default Search
